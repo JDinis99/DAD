a890b6b5585bf68f248abb1a9715d8c19d7c2cfa
mr-dominguez <henrique.dominguez@tecnico.ulisboa.pt>
2020-12-02 22:37:23 +0000

    Performance scripts

---
+++ GigaClient/client_base.ps1
@@ -0,0 +1,1 @@
+Measure-Command { dotnet run 5 false "s1,https://localhost:5001;s2,https://localhost:5002;s3,https://localhost:5003;s4,https://localhost:5004;s5,https://localhost:5005;" performance_client.txt }
